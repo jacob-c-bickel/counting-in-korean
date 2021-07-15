f725233ce6fbcd41590b34013441dfe36714abd9
Jacob Bickel <jacob.c.bickel@gmail.com>
2021-07-15 18:20:29 -0400

    menu

--- app/components/modules/Menu/Menu.js
+++ app/components/modules/Menu/Menu.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useContext, useEffect, useState } from "react";
 
 import styles from "./Menu.module.scss";
 import AppContext from "@app/AppContext";
 
 export default function Menu() {
+  const router = useRouter();
   const app = useContext(AppContext);
-
   const [removeMenu, setRemoveMenu] = useState(true);
 
   useEffect(() => {
@@ -27,10 +28,56 @@ export default function Menu() {
     <>
       <div className={styles.MenuBg + " " + classes} onClick={() => app.setShowMenu(false)} />
       <div className={styles.Menu + " " + classes}>
-        <p>Menu</p>
-        <Link href="/">Home</Link>
-        <Link href="/practice/basic">Basic Practice</Link>
+        <Link href="/">
+          <a className={styles.title} onClick={handleLinkClick} tabIndex="0">
+            Counting in Korean!
+          </a>
+        </Link>
+        <p className={styles.subtitle}>Practice</p>
+
+        <Link href="/practice/basic">
+          <a className={getLinkClassName("/practice/basic")} onClick={handleLinkClick}>
+            Basic Practice
+          </a>
+        </Link>
+        <Link href="/practice/context">
+          <a className={getLinkClassName("/practice/context")} onClick={handleLinkClick}>
+            Context Practice
+          </a>
+        </Link>
+        <p className={styles.subtitle}>Learn</p>
+        <Link href="/learn/native">
+          <a className={getLinkClassName("/learn/native")} onClick={handleLinkClick}>
+            Native Number Reference
+          </a>
+        </Link>
+        <Link href="/learn/sino">
+          <a className={getLinkClassName("/learn/sino")} onClick={handleLinkClick}>
+            Sino Number Reference
+          </a>
+        </Link>
+        <Link href="/learn/usage">
+          <a className={getLinkClassName("/learn/usage")} onClick={handleLinkClick}>
+            Number System Usage
+          </a>
+        </Link>
+        {/* <Link href="/learn/counters">
+          <a className={getLinkClassName("/learn/counters")} onClick={handleLinkClick}>
+            Counter Reference
+          </a>
+        </Link> */}
+        <footer className={styles.footer}>Created by Jacob Bickel, 2021</footer>
       </div>
     </>
   );
+
+  function getLinkClassName(path) {
+    let className = styles.link;
+    if (router.pathname === path) className += " " + styles.active;
+    return className;
+  }
+
+  function handleLinkClick() {
+    app.setShowMenu(false);
+  }
 }
--- app/components/modules/Page/Page.js
+++ app/components/modules/Page/Page.js
@@ -7,9 +7,9 @@ export default function PageContent({ children }) {
   return (
     <motion.div
       className={styles.Page}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
+      // initial={{ opacity: 0 }}
+      // animate={{ opacity: 1 }}
+      // exit={{ opacity: 0 }}
       transition={{ duration: 0.125, ease: "easeInOut" }}
     >
       {children}
